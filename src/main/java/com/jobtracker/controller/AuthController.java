5e13b1dc072baacd8840af542b2365869a4692e9
Nayan <nayandahare02@gmail.com>
2026-04-01 16:34:57 +0530

    Initial commit DAY-2T1

---
+++ src/main/java/com/jobtracker/controller/AuthController.java
@@ -0,0 +1,36 @@
+package com.jobtracker.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jobtracker.entity.User;
+import com.jobtracker.service.UserService;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin
+public class AuthController {
+
+	@Autowired
+	private UserService userService;
+	
+	// REGISTER API
+	@PostMapping("/register")
+	public User register(@RequestBody User user) {
+		System.out.println("AuthController.register()");
+		return userService.registerUser(user);
+	}
+	
+	// LOGIN API
+	@PostMapping("/login")
+	public User login(@RequestParam String email,
+					  @RequestParam String password) {
+		System.out.println("AuthController.login()");
+		return userService.loginUser(email, password);
+	}
+}
---
+++ src/main/java/com/jobtracker/service/UserService.java
@@ -0,0 +1,39 @@
+package com.jobtracker.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jobtracker.entity.User;
+import com.jobtracker.repository.UserRepository;
+
+@Service
+
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	// REGISTER
+	public User registerUser(User user) {
+		return userRepository.save(user);
+	}
+	
+	// LOGIN
+	public User loginUser(String email, String password) {
+		Optional<User> user = userRepository.findByEmail(email);
+		
+		if(user.isPresent() && user.get().getPassword().equals(password)) {
+			return user.get();
+		} else {
+			throw new RuntimeException("Invalid email or password");
+		}
+	}
+}
+
+
+//<dependency>
+//<groupId>org.springframework.boot</groupId>
+//<artifactId>spring-boot-starter-security</artifactId>
+//</dependency>
